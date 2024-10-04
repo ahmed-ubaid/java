d4b8c93c099c6ef65a1469c34fa4a49a608cb273
Ubaid Ahmed <92600722+ahmed-ubaid@users.noreply.github.com>
2024-10-05 01:33:58 +0530

    Add files via upload

--- Stack_and_Queue/src/Main.java
+++ Stack_and_Queue/src/Main.java
@@ -2,16 +2,22 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        DynamicStack m=new DynamicStack(5);
-        m.printList();
+//        DynamicStack m=new DynamicStack(5);
+//        m.printList();
+//
+//        System.out.println("\n"+m.pop());
+//        m.printList();
+//
+//        m.push(8);
+//        m.printList();
+//
+//        System.out.println("\n"+m.peek());
 
-        System.out.println("\n"+m.pop());
-        m.printList();
-
-        m.push(8);
-        m.printList();
-
-        System.out.println("\n"+m.peek());
-
+        DynamicQueue j=new DynamicQueue(5);
+        j.printList();
+        j.dequeue();
+        j.printList();
+        j.enqueue(9);
+        j.printList();
     }
 }
