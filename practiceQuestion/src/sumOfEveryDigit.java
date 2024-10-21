d52a6787ecef11cf94aac551cb423cad5ed40c26
Ubaid Ahmed <92600722+ahmed-ubaid@users.noreply.github.com>
2024-10-22 01:01:52 +0530

    Add files via upload

---
+++ practiceQuestion/src/sumOfEveryDigit.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class sumOfEveryDigit {
+    public static int sumDigits(int n){
+        int sum=0;
+
+        while(n>0){
+            sum+=n%10;
+            n=n/10;
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args){
+
+        Scanner sc=new Scanner(System.in);
+
+        boolean h=true;
+
+        while(h){
+            System.out.println("Enter 1 to play again: ");
+            int k=sc.nextInt();
+
+            h= (k==1);
+
+            System.out.println("Enter the number");
+            int num= sc.nextInt();
+            System.out.println(sumDigits(num));
+        }
+
+    }
+
+}
