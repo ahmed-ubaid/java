70d97b951b92e49dc95d480fd49132e90a8bbe56
Ubaid Ahmed <92600722+ahmed-ubaid@users.noreply.github.com>
2024-10-22 01:03:30 +0530

    Add files via upload

--- practiceQuestion/src/sumOfEveryDigit.java
+++ practiceQuestion/src/sumOfEveryDigit.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class sumOfEveryDigit {
     public static int sumDigits(int n){
+        if(n<0){
+            return -1;
+        }
         int sum=0;
 
         while(n>0){
